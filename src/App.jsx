4d00e8ded0412ae6c301575426436f900f7e9c6d
Katonzi <katonzidenis@gmail.com>
2026-08-06 18:09:22 +0100

    Une nouvelle modification de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useContext } from 'react';
 
-import { AuthContext } from './context/AuthContext';
+import { AuthContext } from '../src/context/authContext';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
